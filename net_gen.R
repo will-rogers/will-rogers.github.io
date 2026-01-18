4b23207710f71381a84526526c200c808448545e
Will Rogers <rogerswill47@gmail.com>
2026-01-18 16:39:53 -0500

    updating netwotk

--- net_gen.R
+++ net_gen.R
@@ -3,7 +3,29 @@ library("devtools")
 install_github("pablobarbera/scholarnetwork")
 
 library(scholarnetwork)
-d <- extractNetwork(id="TWc4vkkAAAAJ", n=500)
-str(d)
+
+pubs <- scholar::get_publications(id = "TWc4vkkAAAAJ")
+pubs <- pubs[-12,]
+
+edges <- lapply(pubs$author, scholarnetwork:::extractAuthors)
+edges <- do.call(rbind, edges)
+edges <- aggregate(edges$weight, by = list(node1 = edges$node1, 
+                                           node2 = edges$node2), FUN = function(x) sum(x))
+names(edges)[3] <- "weight"
+network <- igraph::graph.edgelist(as.matrix(edges[, c("node1", 
+                                                      "node2")]), directed = FALSE)
+igraph::edge_attr(network, "weight") <- edges$weight
+
+network <- decompose(network)[[1]]
+
+fc <- igraph::walktrap.community(network)
+nodes <- data.frame(label = igraph::V(network)$name, degree = igraph::strength(network), 
+                    group = fc$membership, stringsAsFactors = F)
+nodes <- nodes[order(nodes$label), ]
+
+edges <- edges[edges$node1 %in% nodes$label & edges$node2 %in% 
+                   nodes$label, ]
+
+d <- list(nodes = nodes, edges = edges)
 plotNetwork(d$nodes, d$edges, file="/Users/willrogers/Downloads/portfolio/network.html", 
-            fontsize = 15, width = 1600, height = 6000)
+            fontsize = 15, width = 1000, height = 1000, charge = -100)
